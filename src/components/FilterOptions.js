c675adc70d103216ee382355398d78dadb6555a0
Durga Singh <singhdurga5698@gmail.com>
2024-06-16 02:03:59 +0530

    filter by category, source & date is done

--- src/components/FilterOptions.js
+++ src/components/FilterOptions.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setArticles } from '../store/actions';
-import { fetchNewsArticles } from '../services/newsAPIService';
-import { fetchGuardianArticles } from '../services/guardianAPIService';
-import { fetchNYTArticles } from '../services/nytAPIService';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setArticles } from "../store/actions";
+import { fetchNewsArticles } from "../services/newsAPIService";
+import { fetchGuardianArticles } from "../services/guardianAPIService";
+import { fetchNYTArticles } from "../services/nytAPIService";
 
 const FilterOptions = () => {
   const [filterType, setFilterType] = useState("");
@@ -23,23 +23,59 @@ const FilterOptions = () => {
     let allArticles = [];
 
     if (filterType === "date") {
-      const newsAPIArticles = await fetchNewsArticles(date );
+      const newsAPIArticles = await fetchNewsArticles(date);
       const guardianArticles = await fetchGuardianArticles(date);
-      const nytArticles = await fetchNYTArticles(date );
+      const nytArticles = await fetchNYTArticles(date);
       allArticles = [...newsAPIArticles, ...guardianArticles, ...nytArticles];
     } else if (filterType === "category") {
-      const guardianArticles = await fetchGuardianArticles( category );
-      const nytArticles = await fetchNYTArticles( category );
+      const guardianArticles = await fetchGuardianArticles(category);
+      const nytArticles = await fetchNYTArticles(category);
       allArticles = [...guardianArticles, ...nytArticles];
     } else if (filterType === "source") {
-      const newsAPIArticles = await fetchNewsArticles( source );
-      const guardianArticles = await fetchGuardianArticles( source );
-      const nytArticles = await fetchNYTArticles( source );
-      allArticles = [...newsAPIArticles, ...guardianArticles, ...nytArticles];
+      const newsAPIArticles = await fetchNewsArticles(source);
+      const nytArticles = await fetchNYTArticles(source);
+      allArticles = [...newsAPIArticles, ...nytArticles];
     }
 
-    dispatch(setArticles(allArticles));
+    let filteredArticles = [];
+
+    if (filterType === "date") {
+      filteredArticles = allArticles.filter((article) => {
+        const articleDate = formatDate(
+          article.webPublicationDate || article.publishedAt || article.pub_date
+        );
+        return articleDate === date;
+      });
+    } else if (filterType === "source") {
+      filteredArticles = allArticles.filter((article) => {
+        const articleSource =
+          typeof article.source === "string"
+            ? article.source.toLowerCase()
+            : article.source &&
+              typeof article.source === "object" &&
+              article.source.name
+            ? article.source.name.toLowerCase()
+            : "";
+        return articleSource === source.toLowerCase();
+      });
+    }else if (filterType === "category") {
+      filteredArticles = allArticles.filter((article) => {
+        const articleCategory = (typeof article.sectionName === "string" && article.sectionName.toLowerCase()) || (typeof article.news_desk === "string" && article.news_desk.toLowerCase())
+        return articleCategory === category;
+      });
+    }
+
+    dispatch(setArticles(filteredArticles));
   };
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
 
   return (
     <div className="p-4">
