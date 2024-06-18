c6c4d2237c5ddae4763babdda9c01dcd9b5f5401
Durga Singh <singhdurga5698@gmail.com>
2024-06-18 14:41:16 +0530

    Removed unwanted files

--- src/apis/apikeys.js
+++ src/apis/apikeys.js
@@ -3,9 +3,3 @@ export const news_api = "https://newsapi.org/v2";
 export const guardian_api = "https://content.guardianapis.com/search";
 
 export const new_york_api = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-
-// "https://content.guardianapis.com/search?q=election&api-key=b2046261-6169-44b0-a4c6-6424732cb1fd"
-
-// "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=iq8FWFkiNvGZtbqqv1pGreL2GvTLM85F"
-
-// "https://newsapi.org/v2/everything?q=election&apiKey=c16b1b19b1cb4306bde988c7cacec6b8"
