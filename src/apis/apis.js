37d81251f33c9ccc3dcd71bb39d5dc3ca2ff1b95
Durga Singh <singhdurga5698@gmail.com>
2024-06-18 14:10:06 +0530

    deleted unwanted file

--- src/apis/apis.js
+++
@@ -1,5 +0,0 @@
-export const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
-
-export const GUARDIAN_API_KEY = "b2046261-6169-44b0-a4c6-6424732cb1fd"
-
-export const NEW_YORK_API_KEY = "iq8FWFkiNvGZtbqqv1pGreL2GvTLM85F"
